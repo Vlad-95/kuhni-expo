afd124f61bc7905deb7e170509091edd8f5088d8
Vladislav <vzv@siluetstudio.com>
2022-04-22 16:50:16 +0300

    blog_one page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -92,4 +92,16 @@ $(document).ready(function() {
             nextArrow: '<button type="button" class="slider__next"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
         });
     }
+
+    //Новость детальная - слайдер
+    if ($('.blog-detail .slider').length) {
+        $('.slider').slick({
+            arrows: true,
+            dots: false,
+            prevArrow: '<button type="button" class="slider__prev"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
+            nextArrow: '<button type="button" class="slider__next"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
+        })
+
+        $('.slider .slider__item .image').matchHeight();
+    }
 });
